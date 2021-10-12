906bb863e2b5fbf6cc2ea52ee5732285c0951225
Devin Smith <devinsmith@deephaven.io>
2021-10-12 10:29:16 -0700

    fix compression example

--- 20211012/read-csv/12-compression.groovy
+++ 20211012/read-csv/12-compression.groovy
@@ -1,23 +1,2 @@
-import io.deephaven.db.tables.utils.csv.CsvSpecs
-import io.deephaven.db.tables.utils.csv.InferenceSpecs
+x = CsvHelpers.readCsv('/data/some-file.zip')
 
-
-my_inference = InferenceSpecs.builder().addParsers(
-    Parser.INT,
-    Parser.LONG,
-    MyCustomParser,
-    Parser.STRING).build()
-
-the_defaults = CsvSpecs.builder()
-    .inference(InferenceSpecs.standardTimes())
-    .hasHeaderRow(true)
-    .delimiter(',' as char)
-    .quote('"' as char)
-    .ignoreSurroundingSpaces(true)
-    .trim(false)
-    .charset(java.nio.charset.StandardCharsets.UTF_8)
-    .build()
-
-custom_parsers = CsvSpecs.builder()
-    .putParsers("MyColumn", myCustomParser)
-    .build()
